97cca0f15bab7f43ec732b2c864990b565e687e8
arko24 <62396421+arko24@users.noreply.github.com>
2022-08-06 19:42:45 +0700

    Update smart.py

--- pages/smart.py
+++ pages/smart.py
@@ -3,6 +3,21 @@ from bokeh.models.widgets import Div
 
 st.set_page_config(page_title='BCAS KONSUMER', page_icon='🖖',layout="wide")
 
+def add_bg_from_url():
+    st.markdown(
+        f"""
+        <style>
+        .stApp{{
+            background-image: url(https://github.com/arko24/biadian.github.io/blob/main/main.jpg?raw=true);
+            background-attachment: fixed;
+            background-size: cover
+        }}
+        </style>
+        """,
+        unsafe_allow_html=True
+    )
+add_bg_from_url()
+
 hide_menu = """
         <style>
         #MainMenu {visibility: hidden;}
