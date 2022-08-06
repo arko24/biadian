69e788e3d9150e21f1da36ba0a8c7e33035f3d3e
arko24 <62396421+arko24@users.noreply.github.com>
2022-08-06 20:27:55 +0700

    Update smart.py

--- pages/smart.py
+++ pages/smart.py
@@ -1,7 +1,7 @@
 import streamlit as st
 from bokeh.models.widgets import Div
 
-st.set_page_config(page_title='BCAS KONSUMER', page_icon='🖖',layout="wide")
+st.set_page_config(page_title='BCAS KONSUMER',layout="wide")
 
 def add_bg_from_url():
     st.markdown(
@@ -18,10 +18,11 @@ def add_bg_from_url():
     )
 add_bg_from_url()
 
+st.markdown("<h2 style='text-align: center; color: black;'>Log In </h2>", unsafe_allow_html=True)
 hide_menu = """
         <style>
         #MainMenu {visibility: hidden;}
-        footer,header {visibility: hidden;} 
+        footer,header  {visibility: hidden;}
         </style>
         """
 st.markdown(hide_menu, unsafe_allow_html=True)
