41072ca3ec643e07de32c3a26b4cc1f63975419d
arko24 <62396421+arko24@users.noreply.github.com>
2022-08-06 19:41:48 +0700

    Update app.py

--- app.py
+++ app.py
@@ -2,6 +2,21 @@ import streamlit as st
 import streamlit.components.v1 as components
 from bokeh.models.widgets import Div
 
+def add_bg_from_url():
+    st.markdown(
+        f"""
+        <style>
+        .stApp{{
+            background-image: url(https://github.com/arko24/biadian.github.io/blob/main/main.jpg?raw=true);
+            background-attachment: fixed;
+            background-size: cover
+        }}
+        </style>
+        """,
+        unsafe_allow_html=True
+    )
+add_bg_from_url()
+
 st.set_page_config(layout="wide")
 
 st.markdown("<h2 style='text-align: center; color: black;'>Log In </h2>", unsafe_allow_html=True)
