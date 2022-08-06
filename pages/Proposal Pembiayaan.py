b6889a5f41df901ce7b16bb5700611af720809ec
arko24 <62396421+arko24@users.noreply.github.com>
2022-08-06 23:57:17 +0700

    Add files via upload

---
+++ pages/Proposal Pembiayaan.py
@@ -0,0 +1,47 @@
+import streamlit as st
+from bokeh.models.widgets import Div
+
+
+st.set_page_config(page_title='BCAS KONSUMER', page_icon='🖖',layout="wide")
+st.sidebar.image("/home/rumaisha/Desktop/mock/main.jpg",use_column_width=True)
+
+def add_bg_from_url():
+    st.markdown(
+        f"""
+        <style>
+        .stApp{{
+            background-image: url(https://github.com/arko24/biadian.github.io/blob/main/main.jpg?raw=true);
+            background-attachment: fixed;
+            background-size: cover
+        }}
+        </style>
+        """,
+        unsafe_allow_html=True
+    )
+add_bg_from_url()
+
+
+hide_menu = """
+        <style>
+        #MainMenu {visibility: hidden;}
+        footer,header {visibility: hidden;} 
+        </style>
+        """
+st.markdown(hide_menu, unsafe_allow_html=True)
+
+st.text('Input Data Nasabah Pembiayaan')
+title = st.text_input('Nama Nasabah', '')
+title = st.text_input('Nik Nasabah', '')
+title = st.text_input('Alamat', '')
+title = st.text_input('No Telpon', '')
+option = st.selectbox(
+     'Pengajuan Pembiayan',
+     ('Emas', 'KKB', 'KPR'))
+
+st.write('Pilihan Pembiayaan:', option)
+
+uploaded_files = st.file_uploader("Chose File", accept_multiple_files=True)
+for uploaded_file in uploaded_files:
+     bytes_data = uploaded_file.read()
+     st.write("filename:", uploaded_file.name)
+     st.write(bytes_data)
