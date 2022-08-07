60e8df22dfda452d8bfd5dcf87f69f8913caa009
arko24 <62396421+arko24@users.noreply.github.com>
2022-08-07 21:58:29 +0700

    Add files via upload

--- pages/Tracking System.py
+++ pages/Tracking System.py
@@ -1,9 +1,6 @@
 import streamlit as st
 import streamlit.components.v1 as components
-from requests_html import HTMLSession
-import webbrowser
-import urllib.request
-
+from PIL import Image
 
 st.set_page_config(page_title='BCAS KONSUMER', layout="wide")
 
@@ -30,6 +27,5 @@ hide_menu = """
         """
 st.markdown(hide_menu, unsafe_allow_html=True)
 
-
-get_url= webbrowser.open('https://www.appsheet.com/start/7f4ea332-81b3-45bf-9091-385a5d99cec7')
-st.markdown(get_url)
+image = Image.open('KPR.jpeg')
+st.image(image, width=300,)
